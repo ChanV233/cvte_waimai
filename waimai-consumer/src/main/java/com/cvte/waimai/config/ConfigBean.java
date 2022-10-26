0626941e73d3f6444d7b6070665852ad70e2a87f
chenwei <Cw15217294406>
2022-10-26 09:17:49 +0800

    、

--- waimai-consumer/src/main/java/com/cvte/waimai/config/ConfigBean.java
+++ waimai-consumer/src/main/java/com/cvte/waimai/config/ConfigBean.java
@@ -7,10 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * @author ChanV
- * @create 2021-02-04-22:02
- */
 @Configuration
 public class ConfigBean {
 
